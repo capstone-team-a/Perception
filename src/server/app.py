7a3a2d3a7eee2a0d13324505f9dfadea751f59fd
Marko <bozic@pdx.edu>
2019-10-03 16:38:08 -0700

    bootstrapping backend code with Flask and adding requirements file

--- src/server/app.py
+++ src/server/app.py
@@ -1,1 +1,16 @@
-Placeholder
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+
+@app.route("/submit", methods=['POST'])
+def submit():
+    pass
+
