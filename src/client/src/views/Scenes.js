230b18eb10fad57b7cc3ad42065b5643b77b4c32
plavabe12 <23537510+plavabe12@users.noreply.github.com>
2019-10-10 01:24:23 -0700

    Fixed Formating and added delete function

--- src/client/src/views/Scenes.js
+++ src/client/src/views/Scenes.js
@@ -1,7 +1,6 @@
 // This view is the "scene list", where all of the current project's scenes are displayed.
 // Clicking on a scene will route the application to that scene's edit form, which is
 // the list of captions in that scene.
-
 const m = require('mithril')
 
 const Scene = require('../models/Scene')
@@ -10,14 +9,28 @@ module.exports = {
   view: function() {
     return m('.scenes', [
       m('h1', 'Scenes'),
-      m('button.add-scene', {onclick: Scene.addScene}, 'New Scene'),
+      m('button.add-scene', {
+        onclick: Scene.addScene
+      }, 'New Scene'),
       m('h2', 'List of scenes'),
-      m('.scene-list', Scene.getScenes().map(function(scene) {
-	console.log('scene object', scene)
-	return m('a.scene-list-item', {onclick: function() {
-	  m.route.set(`/edit-scene/${scene.id}`, {captions: scene.captions})}
-	}, `Scene ${scene.id}`)
-      }))
+      m('.scene-list', Scene.getScenes()
+        .map(function(scene) {
+          return m('a.scene-list-item', {
+            onclick: function() {
+              m.route.set(`/edit-scene/${scene.id}`, {
+                captions: scene.captions
+              })
+            }
+          }, `Scene ${scene.id}`)
+        })),
+        m('.scene-list', Scene.getScenes()
+          .map(function(scene) {
+            return m('a.scene-list-item', {
+              onclick: function() {
+                Scene.deleteScene(scene.id)
+              }
+            }, `Delete Scene ${scene.id}`)
+          }))
     ])
   }
 }
