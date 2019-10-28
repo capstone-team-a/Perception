70badd81500d5efc52782d99379a6f31fcc6473c
Marko Bozic <bozic@pdx.edu>
2019-10-27 18:46:44 -0700

    PER-380 Creating functions to get basic EOC and default Preamble styling
    Creating functions to get necessary byte pairs for control commands.

--- src/server/cea_608_encoder/scene_utility.py
+++ src/server/cea_608_encoder/scene_utility.py
@@ -12,3 +12,19 @@ def create_bytes_for_scene_opacity(opacity):
 
 def validate_time_formatting(time):
     pass
+
+
+def get_default_preamble_style_bytes() -> tuple:
+    return 0x10, 0x40
+
+
+def get_resume_caption_loading_bytes() -> tuple:
+    return 0x14, 0x20
+
+
+def get_erase_non_displayed_memory_bytes() -> tuple:
+    return 0x14, 0x2e
+
+
+def get_end_of_caption_bytes() -> tuple:
+    return 0x14, 0x2f
